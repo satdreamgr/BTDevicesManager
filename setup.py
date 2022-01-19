229d9eb069f24a062bcf932a07c083931f230497
Athanasios Oikonomou <athoik@gmail.com>
2022-01-19 22:33:15 +0200

    hardknott: fix translation

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(name='enigma2-plugin-extensions-btdevicesmanager',
 		author='SatDreamGr',
 		package_dir={'Extensions.BTDevicesManager': 'src'},
 		packages=['Extensions.BTDevicesManager'],
-		package_data={'Extensions.BTDevicesManager': ['*.png']},
+		package_data={'Extensions.BTDevicesManager': ['*.png', 'locale/*/LC_MESSAGES/*.mo']},
 		description='Bluetooth Devices Manager',
 		cmdclass=setup_translate.cmdclass,
 	)
